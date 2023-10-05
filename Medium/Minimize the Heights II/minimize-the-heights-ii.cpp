a9518b5f9129bc0f882d3e8e6b9288c93a0b4a25
Shubhamm1211 <146850408+Shubhamm1211@users.noreply.github.com>
2023-10-05 15:51:06 +0530

    minimize-the-heights-ii.cpp

--- Medium/Minimize the Heights II/minimize-the-heights-ii.cpp
+++ Medium/Minimize the Heights II/minimize-the-heights-ii.cpp
@@ -11,12 +11,12 @@ class Solution {
   public:
     int getMinDiff(int arr[], int n, int k) {
         sort(arr, arr+n);
-        int diff = arr[n-1]-arr[0];//assuming no diff is bigger than this
+        int diff = arr[n-1]-arr[0];
         for(int i = 1; i<n; i++)
         {
             if(arr[i]-k<0) continue;
-            int mini = min(arr[0]+k, arr[i]-k); //trying to make all the towers except first tower to be min
-            int maxi = max(arr[i-1]+k, arr[n-1]-k); //trying to make all the towers except last tower to be max
+            int mini = min(arr[0]+k, arr[i]-k); 
+            int maxi = max(arr[i-1]+k, arr[n-1]-k); 
             diff = min(diff, maxi-mini);
         }
         return diff;
