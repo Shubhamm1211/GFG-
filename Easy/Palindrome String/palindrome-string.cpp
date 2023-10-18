4abff4644911e997ce4e0d4438271c9a30fee708
Shubhamm1211 <146850408+Shubhamm1211@users.noreply.github.com>
2023-10-18 20:02:02 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Palindrome String/palindrome-string.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+ 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	
+	
+	int isPalindrome(string s)
+	{
+	    // Your code goes here
+	    int start=0;
+	    int end=s.size()-1;
+	    bool flag=true;
+	    while(start<=end){
+	        if(s[start]!=s[end]){
+	            flag=false;
+	        }
+	        start++;
+	        end--;
+	    }
+	    if(flag){
+	        return 1;
+	    }
+	    else{
+	        return 0;
+	    }
+	}
+
+};
+
+
+//{ Driver Code Starts.
+
+int main() 
+{
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		string s;
+   		cin >> s;
+
+   	    Solution ob;
+
+   		cout << ob.isPalindrome(s) << "\n";
+   	}
+
+    return 0;
+}
+// } Driver Code Ends
