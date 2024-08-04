f2777ef653a77d022b1f8ad049ccb5d1f3d6adb9
Shubham Agrawal <146850408+Shubhamm1211@users.noreply.github.com>
2024-08-04 17:47:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Most frequent word in an array of strings/most-frequent-word-in-an-array-of-strings.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution
+{
+    public:
+    //Function to find most frequent word in an array of strings.
+    string mostFrequentWord(string arr[], int n) 
+    {
+        // code here
+        map<string,int>mp;
+        int cnt=0;
+        string ans="";
+        for(int i=n-1;i>=0;i--){
+            mp[arr[i]]++;
+            int t=mp[arr[i]];
+            if(mp[arr[i]]>cnt){
+                cnt=t;
+                ans=arr[i];
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        string arr[n];
+        for (int i = 0; i < n; i++) cin >> arr[i];
+        Solution obj;
+        cout << obj.mostFrequentWord(arr, n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
