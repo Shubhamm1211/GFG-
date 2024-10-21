f2ebe951ca589a4d3608f4906ae1895ead1b674f
Shubham Agrawal <146850408+Shubhamm1211@users.noreply.github.com>
2024-10-21 21:18:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Common Substring/longest-common-substring.cpp
+++ Difficulty: Medium/Longest Common Substring/longest-common-substring.cpp
@@ -4,37 +4,29 @@ using namespace std;
 
 
 // } Driver Code Ends
-class Solution{
-    public:
-    int solve(int ind1,int ind2,string &text1,string &text2,vector<vector<int>>&dp){
-        if(ind1<0 || ind2<0)return 0;
-        if(dp[ind1][ind2]!=-1)return dp[ind1][ind2];
-        if(text1[ind1]==text2[ind2]){
-            return dp[ind1][ind2]=1+solve(ind1-1,ind2-1,text1,text2,dp);
-        }
-        else dp[ind1][ind2]=0;
-        solve(ind1-1,ind2,text1,text2,dp);
-        solve(ind1,ind2-1,text1,text2,dp);
-        return dp[ind1][ind2];
-                                 
-    }
-    int longestCommonSubstr (string text1, string text2)
-    {
-        // your code here
-        int l1=text1.size();
-        int l2=text2.size();
-        vector<vector<int>>dp(l1,vector<int>(l2,-1));
-        int ans=0;
-        for(int i=0;i<l1;i++){
-            for(int j=0;j<l2;j++){
-                solve(i,j,text1,text2,dp);
-                ans=max(ans,dp[i][j]);
-            }
-        }
-        return ans;
+class Solution {
+  public:
+    int solve(int i, int j, string &s, string &t, vector<vector<int>> &dp, int &ans){
+    if(i < 0 || j < 0) return 0;
+    if (dp[i][j] != -1) return dp[i][j];
+     int len = 0;
+    if (s[i] == t[j]) len = 1 + solve(i - 1, j - 1, s, t, dp, ans);
+     ans = max(ans, len);
+    solve(i - 1, j, s, t, dp, ans);
+    solve(i, j - 1, s, t, dp, ans);
+    return dp[i][j] = len;
+}
+
+public:
+    int longestCommonSubstr(string str1, string str2) {
+        int ans = 0;
+    int n = str1.length();
+    int m = str2.length();
+    vector<vector<int>> dp(n, vector<int>(m, -1));
+    solve(n-1, m-1, str1, str2, dp, ans);
+    return ans;
     }
 };
-
 
 //{ Driver Code Starts.
 
