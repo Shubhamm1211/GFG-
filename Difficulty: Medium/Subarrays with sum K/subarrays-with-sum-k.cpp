e37fec4c095450730c5cc3d5e04a693962e1e0d8
Shubham Agrawal <146850408+Shubhamm1211@users.noreply.github.com>
2025-01-03 01:02:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subarrays with sum K/subarrays-with-sum-k.cpp
+++ Difficulty: Medium/Subarrays with sum K/subarrays-with-sum-k.cpp
@@ -1,45 +1,61 @@
 //{ Driver Code Starts
+// Initial Template for C++
 #include <bits/stdc++.h>
 using namespace std;
 
-// } Driver Code Ends
-//User function Template for C++
 
-class Solution{
-    public:
-    int findSubArraySum(int arr[], int n, int k)
-    {
-        // code here
-        unordered_map<int,int>mp;
-        mp[0]=1;
-        int sum=0,ans=0;
-        for(int i=0;i<n;i++){
-            sum+=arr[i];
-            int rem=sum-k;
-            ans=ans+mp[rem];
-            mp[sum]++;
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+int countSubarrays(vector<int>& arr, int k) {
+    int n = arr.size();
+    unordered_map<int, int> mp; 
+    mp[0] = 1;
+    int prefixSum = 0, ans = 0;
+
+    for (int i = 0; i < n; i++) {
+        prefixSum += arr[i];
+        if (mp.find(prefixSum - k) != mp.end()) {
+            ans += mp[prefixSum - k];
         }
-        return ans;
+        mp[prefixSum]++;
     }
+
+    return ans;
+}
+
 };
 
 //{ Driver Code Starts.
-int main()
-{
+
+int main() {
+
     int t;
-    cin>>t;
-    while(t--)
-    {
-        int N;
-        cin >> N;
-        int Arr[N];
-        for(int i=0;i<N;i++)    
-            cin>>Arr[i];
-        int k;
-        cin>>k;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
         Solution obj;
-        cout<<obj.findSubArraySum(Arr, N, k)<<endl;
+        cout << obj.countSubarrays(arr, k);
+        cout << endl;
+        cout << "~"
+             << "\n";
     }
+
     return 0;
 }
+
 // } Driver Code Ends
