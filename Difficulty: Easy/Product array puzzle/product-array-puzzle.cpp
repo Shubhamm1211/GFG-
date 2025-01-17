5a3cfa920aec26f688bb69525468538c5b6b8bc9
Shubham Agrawal <146850408+Shubhamm1211@users.noreply.github.com>
2025-01-17 12:38:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Product array puzzle/product-array-puzzle.cpp
+++ Difficulty: Easy/Product array puzzle/product-array-puzzle.cpp
@@ -1,80 +1,81 @@
 //{ Driver Code Starts
-// Initial template for C++
-
 #include <bits/stdc++.h>
 using namespace std;
 
 
 // } Driver Code Ends
 // User function template for C++
-#define ll long long 
+
 class Solution {
   public:
-    // nums: given vector
-    // return the Product vector P that hold product except self at each index
-    vector<long long int> productExceptSelf(vector<long long int>& nums) {
-
+    vector<int> productExceptSelf(vector<int>& arr) {
         // code here
-        long long int cnt0=0;
-        for(auto x:nums){
-            if(x==0)cnt0++;
+        int n = arr.size();
+        int cnt0 = 0;
+        for(int i = 0; i < n; i++){
+            if(arr[i] == 0) cnt0++;
         }
-        if(cnt0==0){
-            ll pro=1;
-            for(auto x:nums)pro=pro*x;
-            vector<ll>ans;
-            for(auto x:nums){
-                ans.push_back(pro/x);
-            }
+        if(cnt0 >= 2){
+            vector <int> ans(n,0);
             return ans;
         }
-        else if(cnt0==1){
-            ll pro=1;
-            for(auto x:nums){
-                if(x==0)continue;
-                else{
-                    pro=pro*x;
+        if(cnt0 == 1){
+            int pro = 1;
+            int ind = -1;
+            for(int i = 0; i < n; i++){
+                if(arr[i] == 0){
+                    ind = i;
+                    continue;
                 }
+                pro *= arr[i];
             }
-            vector<ll>ans(nums.size());
-            for(ll i=0;i<nums.size();i++){
-                if(nums[i]==0){
-                    ans[i]=pro;
-                }
-                else ans[i]=0;
-            }
+            vector <int> ans(n,0);
+            ans[ind] = pro;
             return ans;
         }
-        else{
-            vector<ll>ans(nums.size(),0);
-            return ans;
+        vector <int> ans(n);
+        int pro = 1;
+        for(int i = 0; i < n; i++){
+            pro *= arr[i];
         }
+        for(int i = 0; i < n; i++){
+            ans[i] = pro / arr[i];
+        }
+        return ans;
     }
 };
 
 
 //{ Driver Code Starts.
+
 int main() {
-    int t; // number of test cases
+
+    int t;
     cin >> t;
+    cin.ignore();
+
     while (t--) {
-        int n; // size of the array
-        cin >> n;
-        vector<long long int> arr(n), vec(n);
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
 
-        for (int i = 0; i < n; i++) // input the array
-        {
-            cin >> arr[i];
+        while (ss >> number) {
+            arr.push_back(number);
         }
-        Solution obj;
-        vec = obj.productExceptSelf(arr); // function call
 
-        for (int i = 0; i < n; i++) // print the output
-        {
-            cout << vec[i] << " ";
+        Solution obj;
+        vector<int> res = obj.productExceptSelf(arr);
+
+        for (int i = 0; i < res.size(); i++) {
+            cout << res[i] << " ";
         }
         cout << endl;
+        cout << "~\n";
     }
+
     return 0;
 }
+
 // } Driver Code Ends
