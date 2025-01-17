9e8a41ebe4add97ea540dc4a5ef3f5313fa09d37
Shubham Agrawal <146850408+Shubhamm1211@users.noreply.github.com>
2025-01-17 23:49:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Print N-bit binary numbers having more 1s than 0s/print-nbit-binary-numbers-having-more-1s-than-0s.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+    vector <string> ans;
+    void solve(int n, string s, int cnt1, int cnt0){
+        if(n == 0){
+            ans.push_back(s);
+            return;
+        }
+        if(cnt1 >= cnt0){
+            solve(n - 1, s + '1', cnt1 + 1, cnt0);
+        }
+        if(cnt1 > cnt0){
+            solve(n - 1,s + '0' ,cnt1, cnt0 + 1);
+        }
+    }
+	vector<string> NBitBinary(int n)
+	{
+	    // Your code goes here
+	    ans.clear();
+	    int cnt1 = 0, cnt0 = 0;
+	    string s = "";
+	    solve(n,"",cnt1,cnt0);
+	    return ans;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+   	
+
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		int n;
+   		cin >> n;
+        Solution ob;
+   		vector<string> ans = ob.NBitBinary(n);
+
+   		for(auto i:ans)
+   			cout << i << " ";
+
+   		cout << "\n";
+   	
+cout << "~" << "\n";
+}
+
+    return 0;
+}
+// } Driver Code Ends
