678ad66adb12fa2f40748d38bae25cf0306efbd1
Shubham Agrawal <146850408+Shubhamm1211@users.noreply.github.com>
2025-02-23 15:55:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Next Greater Element/next-greater-element.cpp
+++ Difficulty: Medium/Next Greater Element/next-greater-element.cpp
@@ -12,20 +12,19 @@ class Solution {
         stack <int> st;
         int n = arr.size();
         vector <int> ans(n);
-        for(int i = n - 1; i >= 0; i--){
+        ans[n - 1] = -1;
+        st.push(arr[n - 1]);
+        for(int i = n - 2; i >= 0; i--){
             if(st.empty()){
-                st.push(arr[i]);
                 ans[i] = -1;
+                continue;
             }
-            else{
-                while(!st.empty() and st.top() <= arr[i]){
-                    st.pop();
-                }
-                if(st.empty()) ans[i] = -1;
-                else ans[i] = st.top();
-                st.push(arr[i]);
+            while(!st.empty() and st.top() <= arr[i]){
+                st.pop();
             }
-        
+            if(st.empty()) ans[i] = -1;
+            else ans[i] = st.top();
+            st.push(arr[i]);
         }
         return ans;
     }
